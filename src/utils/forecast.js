cab7e0bbed01c4d54e578fa3044ea215f57e161d
Kapil Pandey <kapil.pandey@enquero.com>
2019-03-13 14:47:47 +0530

    new degree celcius

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,16 +1,15 @@
 const request = require('request')
 const forecast = (long, lat, callback) => {
-    const url = "https://api.darksky.net/forecast/60da98cf84d1a950e397e397e75145ef/" + lat + "," + long+"?unit=si";
-    request({ url, json: true }, (error, {body}) => {
+    const url = "https://api.darksky.net/forecast/60da98cf84d1a950e397e397e75145ef/" + lat + "," + long + "?unit=si";
+    request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback("Unable to connect", undefined)
-        }
-        else if (body.error) {
+        } else if (body.error) {
             callback(body.error, undefined)
-        }
-        else {
-            callback('', "It is currently " + body.currently.temperature +
-                " degree Fahrenheit out. There is " + body.currently.precipProbability +
+        } else {
+            const degreeC = parseInt(((parseInt(body.currently.temperature) - 32) * 5) / 9);
+            callback('', "It is currently " + degreeC +
+                " degree Celcius out there and there are " + body.currently.precipProbability +
                 " chances of rain")
         }
     })
